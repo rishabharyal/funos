230a3a10a452fba476b180e17159bb1d0c9d26f8
Rishabh Aryal <rishabharyal@protonmail.com>
2023-10-12 13:24:52 +0545

    hello world

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// src/main.rs
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info); 
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+
+    loop {}
+}
